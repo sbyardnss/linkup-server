7809e58eb31843bb730c516169ce6321dab14a71
Mary Williams <sbyardnss@gmail.com>
2023-05-12 14:50:57 -0500

    migrated

--- linkupapi/models/course.py
+++ linkupapi/models/course.py
@@ -4,4 +4,4 @@ class Course(models.Model):
     name=models.CharField(max_length=50)
     address=models.CharField(max_length=100)
     phone_number=models.CharField(max_length=25)
-    url=models.CharField(max_length=None)
+    url=models.CharField(max_length=300)
--- linkupapi/models/golfer_match.py
+++ linkupapi/models/golfer_match.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class GolferMatch(models.Model):
-    golfer=models.ForeignKey('Golfer', on_delete=models.CASCADE, related_name='matches')
+    golfer=models.ForeignKey('Golfer', on_delete=models.CASCADE, related_name='golfer')
     match=models.ForeignKey('Match', on_delete=models.CASCADE, related_name='players')
     is_initiator=models.BooleanField()
