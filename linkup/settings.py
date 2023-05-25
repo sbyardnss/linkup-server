4245e1d96fd013c0e1f2f68a732b061628ea5fa6
Mary Williams <sbyardnss@gmail.com>
2023-05-25 13:44:18 -0500

    last commit before switching to deployment

--- linkup/settings.py
+++ linkup/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%!(&*krdda1ttw8(^g!24*0p$uezf^7(*9s(cbhwt3qw+yvib!'
-
+SECRET_KEY = os.environ.get('MY_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
