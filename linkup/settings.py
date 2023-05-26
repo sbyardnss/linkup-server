48d2885a3a9cf8990346320f6c6f349953d300cf
Mary Williams <sbyardnss@gmail.com>
2023-05-26 09:54:15 -0500

    updated

--- linkup/settings.py
+++ linkup/settings.py
@@ -53,21 +53,20 @@ REST_FRAMEWORK = {
     ],
 }
 
+CORS_ALLOW_CREDENTIALS = True
 # THIS IS NEW
-CORS_ORIGIN_WHITELIST = (
+CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
     'https://6470c208d361a40009ceab9e--visionary-treacle-0efacd.netlify.app',
-    'https://deploy--visionary-treacle-0efacd.netlify.app',
-    'CORS_ALLOW_CREDENTIALS = True'
-)
-CORS_ALLOWED_ORIGINS = (
+    'https://deploy--visionary-treacle-0efacd.netlify.app'
+]
+CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
     'https://6470c208d361a40009ceab9e--visionary-treacle-0efacd.netlify.app',
-    'https://deploy--visionary-treacle-0efacd.netlify.app',
-    'CORS_ALLOW_CREDENTIALS = True'
-)
+    'https://deploy--visionary-treacle-0efacd.netlify.app'
+]
 # CORS_ORIGIN_ALLOW_ALL = True
 
 # UPDATE THIS
